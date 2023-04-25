6a1491c1f003096f9d5e93b4b4c35399d1012be0
RaminAtazade <739bgc2@code.edu.az>
2023-04-25 17:23:34 +0400

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+// js arraya elave etme
+// let obj = {
+//     arr :[1,4,8,16],
+//     add : function(){
+//        this.arr[this.arr.length++]=3;
+//     }
+// }
+// obj.add(3)
+// console.log(obj.arr);
+
+
+// arraydan silme
+let obj = {
+    arr :[1,4,8,16],
+    remove : function(){
+       this.arr.length--
+    }
+}
+obj.remove()
+console.log(obj.arr);
